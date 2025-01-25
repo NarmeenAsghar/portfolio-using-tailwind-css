9be07726fd01ef9fa8fd0a31ff9eea029388bb36
NarmeenAsghar <163905451+NarmeenAsghar@users.noreply.github.com>
2025-01-25 21:47:06 +0500

    Updated

--- components/sub/ProjectCard.tsx
+++ components/sub/ProjectCard.tsx
@@ -12,26 +12,22 @@ const ProjectCard = () => {
         <div className="flex flex-col p-4 border border-gray-600 bg-[#030014] transition-transform duration-300 transform hover:scale-105 hover:shadow-lg hover:bg-[#100a31] rounded-2xl">
           <div className="relative w-full h-[12rem] overflow-hidden rounded-lg">
             <Image
-              src={fullstack}
+              src="/hackathon3.png"
               alt="Project 1"
               fill
-              className="object-cover"
             />
           </div>
-          <h2 className="mt-2 text-purple-800 text-xl font-bold text-center">
-            Project 1
+          <h2 className="mt-8 text-purple-800 text-xl font-bold text-center">
+            Hackathon 3 Furniture Website
           </h2>
-          <p className="mt-2 text-sm text-center text-white">
-            Countdown Application
-          </p>
           <div className="flex flex-col xl:flex-row justify-center gap-4 mt-4">
             <Button className="bg-slate-600 rounded-e-xl">
-              <Link href="https://vercel.com/careercrafters-projects/nextjs-countdown-app">
+              <Link href="https://hackathon-3-website-opal.vercel.app/">
                 Live Demo
               </Link>
             </Button>
             <Button className="bg-slate-600 rounded-e-xl">
-              <Link href="https://github.com/Career01crafter/Nextjs-Countdown-app.git">
+              <Link href="https://github.com/NarmeenAsghar/hackathon-3-website.git">
                 Source Code
               </Link>
             </Button>
@@ -42,24 +38,22 @@ const ProjectCard = () => {
         <div className="flex flex-col p-4 border border-gray-600 bg-[#030014] transition-transform duration-300 transform hover:scale-105 hover:shadow-lg hover:bg-[#100a31] rounded-2xl">
           <div className="relative w-full h-[12rem] overflow-hidden rounded-lg">
             <Image
-              src={fullstack}
+              src="/blogWebsite.png"
               alt="Project 2"
               fill
-              className="object-cover"
             />
           </div>
-          <h2 className="mt-4 text-purple-800 text-xl font-bold text-center">
-            Project 2
+          <h2 className="mt-8 text-purple-800 text-xl font-bold text-center">
+            Blog Website
           </h2>
-          <p className="mt-2 text-sm text-center text-white">Portfolio</p>
           <div className="flex flex-col xl:flex-row justify-center gap-4 mt-4">
             <Button className="bg-slate-600 rounded-e-xl">
-              <Link href="https://vercel.com/careercrafters-projects/portfolio">
+              <Link href="https://milestone-3-blog-website-beta.vercel.app/">
                 Live Demo
               </Link>
             </Button>
             <Button className="bg-slate-600 rounded-e-xl">
-              <Link href="https://github.com/Career01crafter/portfolio.git">
+              <Link href="https://github.com/NarmeenAsghar/milestone-3-blog-website.git">
                 Source Code
               </Link>
             </Button>
@@ -70,26 +64,22 @@ const ProjectCard = () => {
         <div className="flex flex-col p-4 border border-gray-600 bg-[#030014] transition-transform duration-300 transform hover:scale-105 hover:shadow-lg hover:bg-[#100a31] rounded-2xl">
           <div className="relative w-full h-[12rem] overflow-hidden rounded-lg">
             <Image
-              src={fullstack}
+              src="/jewelryWebsite.png"
               alt="Project 3"
               fill
-              className="object-cover"
             />
           </div>
-          <h2 className="mt-4 text-purple-800 text-xl font-bold text-center">
-            Project 3
+          <h2 className="mt-8 text-purple-800 text-xl font-bold text-center">
+            Jewelry Website
           </h2>
-          <p className="mt-2 text-sm text-center text-white">
-            Birthday Wish Application
-          </p>
           <div className="flex flex-col xl:flex-row justify-center gap-4 mt-4">
             <Button className="bg-slate-600 rounded-e-xl">
-              <Link href="https://vercel.com/careercrafters-projects/nextjs-birthday-wish-app">
+              <Link href="https://vercel.com/narmeen-asghar/milestone-3-ecommerce-website">
                 Live Demo
               </Link>
             </Button>
             <Button className="bg-slate-600 rounded-e-xl">
-              <Link href="https://github.com/Career01crafter/Nextjs-BirthdayWish-app.git">
+              <Link href="https://github.com/NarmeenAsghar/milestone-3-ecommerce-website.git">
                 Source Code
               </Link>
             </Button>
@@ -100,26 +90,22 @@ const ProjectCard = () => {
         <div className="flex flex-col p-4 border border-gray-600 bg-[#030014] transition-transform duration-300 transform hover:scale-105 hover:shadow-lg hover:bg-[#100a31] rounded-2xl">
           <div className="relative w-full h-[12rem] overflow-hidden rounded-lg">
             <Image
-              src={fullstack}
+              src="/miniWebsite.png"
               alt="Project 4"
               fill
-              className="object-cover"
             />
           </div>
-          <h2 className="mt-4 text-purple-800 text-xl font-bold text-center">
-            Project 4
+          <h2 className="mt-8 text-purple-800 text-xl font-bold text-center">
+            Mini Website
           </h2>
-          <p className="mt-2 text-sm text-center text-white">
-            Number Guessing Game App
-          </p>
           <div className="flex flex-col xl:flex-row justify-center gap-4 mt-4">
             <Button className="bg-slate-600 rounded-e-xl">
-              <Link href="https://vercel.com/careercrafters-projects/nextjs-number-guessing-game">
+              <Link href="https://mini-website-using-custom-css.vercel.app/">
                 Live Demo
               </Link>
             </Button>
             <Button className="bg-slate-600 rounded-e-xl">
-              <Link href="https://github.com/Career01crafter/Nextjs-Number-Guessing-Game.git">
+              <Link href="https://github.com/NarmeenAsghar/mini-website-using-custom-css.git">
                 Source Code
               </Link>
             </Button>
@@ -130,26 +116,22 @@ const ProjectCard = () => {
         <div className="flex flex-col p-4 border border-gray-600 bg-[#030014] transition-transform duration-300 transform hover:scale-105 hover:shadow-lg hover:bg-[#100a31] rounded-2xl">
           <div className="relative w-full h-[12rem] overflow-hidden rounded-lg">
             <Image
-              src={fullstack}
+              src="/birthdayWish.png"
               alt="Project 5"
               fill
-              className="object-cover"
             />
           </div>
-          <h2 className="mt-4 text-purple-800 text-xl font-bold text-center">
-            Project 5
+          <h2 className="mt-8 text-purple-800 text-xl font-bold text-center">
+            Birthday Wish App
           </h2>
-          <p className="mt-2 text-sm text-center text-white">
-            Article On Diabetic Diseases
-          </p>
           <div className="flex flex-col xl:flex-row justify-center gap-4 mt-4">
             <Button className="bg-slate-600 rounded-e-xl">
-              <Link href="https://vercel.com/careercrafters-projects/article-diabetes-html">
+              <Link href="https://nextjs-birthday-wish-app.vercel.app/">
                 Live Demo
               </Link>
             </Button>
             <Button className="bg-slate-600 rounded-e-xl">
-              <Link href="https://github.com/Career01crafter/Article-Diabetes-HTML.git">
+              <Link href="https://github.com/NarmeenAsghar/Nextjs-BirthdayWish-app.git">
                 Source Code
               </Link>
             </Button>
@@ -157,6 +139,32 @@ const ProjectCard = () => {
         </div>
 
         {/* Project Card 6 */}
+        <div className="flex flex-col p-4 border border-gray-600 bg-[#030014] transition-transform duration-300 transform hover:scale-105 hover:shadow-lg hover:bg-[#100a31] rounded-2xl">
+          <div className="relative w-full h-[12rem] overflow-hidden rounded-lg">
+            <Image
+              src="/portfolio.png"
+              alt="Project 5"
+              fill
+            />
+          </div>
+          <h2 className="mt-8 text-purple-800 text-xl font-bold text-center">
+            HTML CSS based Portfolio
+          </h2>
+          <div className="flex flex-col xl:flex-row justify-center gap-4 mt-4">
+            <Button className="bg-slate-600 rounded-e-xl">
+              <Link href="https://portfolio-rosy-seven-86.vercel.app/">
+                Live Demo
+              </Link>
+            </Button>
+            <Button className="bg-slate-600 rounded-e-xl">
+              <Link href="https://github.com/NarmeenAsghar/portfolio.git">
+                Source Code
+              </Link>
+            </Button>
+          </div>
+        </div>
+
+        {/* Project Card 7 */}
         <div className="flex flex-col p-4 border border-gray-600 bg-[#030014] transition-transform duration-300 transform hover:scale-105 hover:shadow-lg hover:bg-[#100a31] rounded-2xl">
           <div className="relative w-full h-[12rem] overflow-hidden rounded-lg">
             <Image
@@ -174,12 +182,12 @@ const ProjectCard = () => {
           </p>
           <div className="flex flex-col xl:flex-row justify-center gap-4 mt-4">
             <Button className="bg-slate-600 rounded-e-xl">
-              <Link href="https://vercel.com/careercrafters-projects">
+              <Link href="https://vercel.com/narmeen-asghar">
                 Vercel
               </Link>
             </Button>
             <Button className="bg-slate-600 rounded-e-xl">
-              <Link href="https://github.com/Career01crafter">Github</Link>
+              <Link href="https://github.com/NarmeenAsghar">Github</Link>
             </Button>
           </div>
         </div>
